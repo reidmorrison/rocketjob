6b18d4a6ed27f16fd15e1bb37e739472e8056693
Reid Morrison <reidmo@gmail.com>
2015-12-09 13:02:09 -0500

    Add cron examples and cron timezone tests

--- lib/rocket_job/concerns/cron.rb
+++ lib/rocket_job/concerns/cron.rb
@@ -16,6 +16,52 @@ module RocketJob
     # - The job will not be restarted if:
     #   - A validation fails after cloning this job.
     #   - The job has expired.
+    #
+    # Example:
+    #
+    # class MyCronJob < RocketJob::Job
+    #   include RocketJob::Concerns::Cron
+    #
+    #   # Set the default cron_schedule
+    #   rocket_job do |job|
+    #     job.cron_schedule = "* 1 * * * UTC"
+    #   end
+    #
+    #   def perform
+    #     puts "DONE"
+    #   end
+    # end
+    #
+    # # Queue the job for processing using the default cron_schedule specified above
+    # MyCronJob.create!
+    #
+    # # Set the cron schedule:
+    # MyCronJob.create!(cron_schedule: '* 1 * * * America/New_York')
+    #
+    #
+    # Note:
+    #
+    # To prevent multiple instances of the job from running at the same time,
+    # add: "include RocketJob::Concerns::Singleton"
+    #
+    # Example: Only allow one instance of the cron job to run at a time:
+    #
+    # class MyCronJob < RocketJob::Job
+    #   # Add `cron_schedule`
+    #   include RocketJob::Concerns::Cron
+    #   # Prevents mutiple instances from being queued or run at the same time
+    #   include RocketJob::Concerns::Singleton
+    #
+    #   # Set the default cron_schedule
+    #   rocket_job do |job|
+    #     job.cron_schedule = "* 1 * * * UTC"
+    #   end
+    #
+    #   def perform
+    #     puts "DONE"
+    #   end
+    # end
+    #
     module Cron
       extend ActiveSupport::Concern
 
--- test/concerns/cron_test.rb
+++ test/concerns/cron_test.rb
@@ -29,26 +29,48 @@ module Concerns
           assert @job.valid?
           refute @job.new_record?
         end
+
+        describe 'timezones are supported' do
+          it 'handles UTC' do
+            time = Time.parse('2015-12-09 17:50:05 +0000')
+            Time.stub(:now, time) do
+              @job = CronJob.create!(cron_schedule: '* 1 * * * UTC')
+            end
+            assert @job.valid?
+            refute @job.new_record?
+            assert_equal Time.parse('2015-12-10 01:00:00 UTC'), @job.run_at
+          end
+
+          it 'handles Eastern' do
+            time = Time.parse('2015-12-09 17:50:05 +0000')
+            Time.stub(:now, time) do
+              @job = CronJob.create!(cron_schedule: '* 1 * * * America/New_York')
+            end
+            assert @job.valid?
+            refute @job.new_record?
+            assert_equal Time.parse('2015-12-10 06:00:00 UTC'), @job.run_at
+          end
+        end
+
       end
 
       describe '#valid?' do
         it 'fails on missing cron schedule' do
           @job = CronJob.new
           refute @job.valid?
-          assert_equal "can't be blank",  @job.errors.messages[:cron_schedule].first
+          assert_equal "can't be blank", @job.errors.messages[:cron_schedule].first
         end
 
         it 'fails on bad cron schedule' do
           @job = CronJob.new(cron_schedule: 'blah')
           refute @job.valid?
-          assert_equal "not a valid cronline : 'blah'",  @job.errors.messages[:cron_schedule].first
+          assert_equal "not a valid cronline : 'blah'", @job.errors.messages[:cron_schedule].first
         end
 
         it 'passes on valid cron schedule' do
           @job = CronJob.new(cron_schedule: '* 1 * * *')
           assert @job.valid?
         end
-
       end
 
     end
