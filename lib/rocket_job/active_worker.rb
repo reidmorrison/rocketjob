0d37050551b8ab25f613f17cdfd6413c8cb91b53
Reid Morrison <reidmo@gmail.com>
2017-02-10 15:00:31 -0500

    Fix ActiveWorker.requeue_zombies

--- lib/rocket_job/active_worker.rb
+++ lib/rocket_job/active_worker.rb
@@ -27,7 +27,7 @@ module RocketJob
     # Requeues all jobs for which the workers have disappeared
     def self.requeue_zombies
       all.each do |active_worker|
-        next if !active_worker.zombie? || !active_worker.job.may_requeue?
+        next if !active_worker.zombie? || !active_worker.job.may_requeue?(active_worker.server_name)
         active_worker.job.requeue!(active_worker.server_name)
       end
     end
