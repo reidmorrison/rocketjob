aeaa855c8cf5d1e0de71a876f6f1b4cbc456f584
Reid Morrison <reidmo@gmail.com>
2019-01-24 15:03:07 -0500

    Add Examples for the Logger Subscriber

--- lib/rocket_job/subscribers/logger.rb
+++ lib/rocket_job/subscribers/logger.rb
@@ -13,7 +13,21 @@ module RocketJob
         @host_name = host_name
       end
 
-      def level(class_name: nil, level: :info, host_name: nil, pid: nil)
+      # Change the log level
+      #
+      # Examples:
+      #   # Change the global log level to :trace on all servers.
+      #   RocketJob::Subscribers::Logger.publish(:set, level: :trace)
+      #
+      #   # Change the global log level to :trace on one server.
+      #   RocketJob::Subscribers::Logger.publish(:set, level: :trace, host_name: 'server1.company.com')
+      #
+      #   # Change the global log level to :trace for a specific process id.
+      #   RocketJob::Subscribers::Logger.publish(:set, level: :trace, host_name: 'server1.company.com', pid: 34567)
+      #
+      #   # Change the log level for a specific class to :trace.
+      #   RocketJob::Subscribers::Logger.publish(:set, level: :trace, class_name: 'RocketJob::Supervisor')
+      def set(level: :info, class_name: nil, host_name: nil, pid: nil)
         return unless for_me?(host_name, pid)
 
         if class_name
@@ -25,6 +39,17 @@ module RocketJob
         end
       end
 
+      # Dump all backtraces to the log file.
+      #
+      # Examples:
+      #   # Thread dump on all servers:
+      #   RocketJob::Subscribers::Logger.publish(:thread_dump)
+      #
+      #   # Change the global log level to :trace on one server.
+      #   RocketJob::Subscribers::Logger.publish(:thread_dump, host_name: 'server1.company.com')
+      #
+      #   # Change the global log level to :trace for a specific process id.
+      #   RocketJob::Subscribers::Logger.publish(:thread_dump, host_name: 'server1.company.com', pid: 34567)
       def thread_dump(host_name: nil, pid: nil)
         return unless for_me?(host_name, pid)
 
